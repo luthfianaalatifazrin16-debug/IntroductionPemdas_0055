ada0e3788b68702c0557f6ba8d8ddb0b782c16aa
luthfianaalatifazrin16-debug <luthfianaalatifazrin16@gmail.com>
2026-02-24 16:03:44 +0700

    pesan

---
+++ luaspersegi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{ // start
+    float p, l, luas;
+
+    // langkah 2 : masukkan panjang
+    cout << "masukkan panjang : ";
+    cin >> p;
+    // langkah 3 : masukkan lebar
+    cout << "masukkan ebar : ";
+    cin >> l;
+    // langkah 4 : Hitung Luas = p x l
+    luas = p * l;
+    // langkah 5
+    cout << "hasilnya = " << luas;
+}
---
+++ pertama.cpp
@@ -0,0 +1,19 @@
+// deklarasi library
+#include <iostream>
+using namespace std;
+
+// program utama
+int min()
+{
+    // deklarasi variable umur
+    int umur;
+    // memberi nilai dari variable
+    umur = 20;
+
+    // menampilkan isi string
+    cout << "abra" << endl;
+    // menampilkan isi integer
+    cout << 78 << endl;
+    // menampilkan isi dari variabel
+    cout << umur << endl;
+}
