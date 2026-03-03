5ebb4a8cf2065b68052d7fe95c1d4386ea4055d5
luthfianaalatifazrin16-debug <luthfianaalatifazrin16@gmail.com>
2026-03-03 15:57:14 +0700

    empat

---
+++ praktikum2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+float p, l;
+
+void input()
+{
+    cout << " Masukkan Panjang : ";
+    cin >> p;
+    cout << " Masukkan Lebar : ";
+    cin >> l;
+}
+
+float LuasPersegi(float x, float y)
+{
+    return x * y;
+}
+
+int Jumlah(int a, int b, int c)
+{
+    return a + b + c;
+}
+
+void output()
+{
+    cout << "Hasilnya = " << LuasPersegi(p, l) << endl;
+}
+int main()
+{
+    input();
+    output();
+    cout << "Hasil Penjumlahan = " << Jumlah(5, 6, 9);
+}
