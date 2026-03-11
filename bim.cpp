528f61133d5c30736f29e41fc5d4a99ad14f3dbd
luthfianaalatifazrin16-debug <luthfianaalatifazrin16@gmail.com>
2026-03-11 11:15:26 +0700

    status berat badan

--- bim.cpp
+++ bim.cpp
@@ -17,3 +17,23 @@ float hitungBMI(float berat, float tinggi)
 {
     return berat / (tinggi * tinggi);
 }
+// Fungsi untuk menentukan status berat badan
+string cekStatus(float bmi)
+{
+    if (bmi < 18.5)
+    {
+        return "Berat Badan Kurang";
+    }
+    else if (bmi < 25)
+    {
+        return "Berat Badan Normal";
+    }
+    else if (bmi < 30)
+    {
+        return "Berat Badan Kelebihan";
+    }
+    else
+    {
+        return "Obesitas";
+    }
+}
