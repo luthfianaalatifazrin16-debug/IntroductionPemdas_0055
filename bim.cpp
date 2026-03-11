71533218cc23ef9e1b143654089039dd58d01fc5
luthfianaalatifazrin16-debug <luthfianaalatifazrin16@gmail.com>
2026-03-11 09:30:11 +0700

    void log data

---
+++ bim.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void masukanData(float &berat, float &tinggi)
+{
+    cout << "Masukan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukan tinggi badan (meter): ";
+    cin >> tinggi;
+}
